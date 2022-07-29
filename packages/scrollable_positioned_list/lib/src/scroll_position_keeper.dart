f61673cf986639e93f69b4aed06b4950af022e15
Yuriy Trofimov <yuriy.trofimov@lanars.com>
2022-07-29 16:05:12 +0300

    Add license to scroll position keeper file

--- packages/scrollable_positioned_list/lib/src/scroll_position_keeper.dart
+++ packages/scrollable_positioned_list/lib/src/scroll_position_keeper.dart
@@ -1,4 +1,8 @@
-import 'package:flutter/material.dart';
+// Copyright 2019 The Fuchsia Authors. All rights reserved.
+// Use of this source code is governed by a BSD-style license that can be
+// found in the LICENSE file.
+
+import 'package:flutter/foundation.dart';
 
 class ScrollPositionKeeper {
   final ValueKey Function(int index) onItemKey;
